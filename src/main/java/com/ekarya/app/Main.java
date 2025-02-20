f44c8e0c6fc681a32b7d770adfc43f71d449be2e
HoussemBettaieb69 <henitogamer@gmail.com>
2025-02-20 01:00:56 +0100

    SignIn Try

--- src/main/java/com/ekarya/app/Main.java
+++ src/main/java/com/ekarya/app/Main.java
@@ -1,6 +1,10 @@
 package com.ekarya.app;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -9,13 +13,20 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException{
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("C:\\Users\\henit\\OneDrive\\Documents\\GitHub\\E-karya-App\\src\\main\\resources\\fxml\\signup.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);/* 
+         * String javaVersion = System.getProperty("java.version");
+         * String javafxVersion = System.getProperty("javafx.version");
+         * Label label = new Label("Hello, JavaFX " + javafxVersion +
+         * ", running on Java " + javaVersion + ".");
+         * Scene scene = new Scene(new StackPane(label), 640, 480);
+         * stage.setScene(scene);*/
+          stage.show();
+         
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ekarya/controller/SignInController.java
@@ -0,0 +1,26 @@
+package com.ekarya.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignInController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void handleSignIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSignUp(ActionEvent event) {
+
+    }
+
+}
