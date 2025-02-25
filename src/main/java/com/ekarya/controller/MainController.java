87477f5baf515042c3619a463858e23343bac5a6
Aziz-Boujlida <azizboujlida93@gmail.com>
2025-02-25 23:18:50 +0100

    Implement click handler

--- src/main/java/com/ekarya/controller/MainController.java
+++ src/main/java/com/ekarya/controller/MainController.java
@@ -1,11 +1,19 @@
 package com.ekarya.controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -49,14 +57,17 @@ public class MainController {
 
     }
 
-    @FXML
-    void handleListingClick(ActionEvent event) {
-
-    }
 
     @FXML
     void handleSearch(ActionEvent event) {
 
     }
-
+    @FXML
+    public void handleListingClick(MouseEvent event) throws IOException{ 
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/propretyDesc.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
 }
+
+
