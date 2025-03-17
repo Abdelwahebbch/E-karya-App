3434e1de15d3b5a3f0dfbba45f4fc0beec97fb92
Aziz-Boujlida <azizboujlida93@gmail.com>
2025-03-17 13:19:33 +0100

    integrate the transitions with the new interface

--- src/main/java/com/ekarya/controller/RoomDetailController.java
+++ src/main/java/com/ekarya/controller/RoomDetailController.java
@@ -1,12 +1,8 @@
 package com.ekarya.controller;
 
-import java.io.IOException;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
